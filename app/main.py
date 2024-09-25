b9cab15116ced96cf9dbdedc2e0046cc439294c6
ManethDulwan <70561561+ManethDulwan@users.noreply.github.com>
2024-09-25 13:52:43 +0530

    last

--- app/main.py
+++ app/main.py
@@ -5,7 +5,6 @@ from Models.models import db, Student
 app = Flask(__name__)
 CORS(app)
 
-# Configure SQLAlchemy
 app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+mysqlconnector://root:@localhost/lms'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db.init_app(app)
